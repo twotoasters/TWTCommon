ababa5ada9949459932da63a60ebc5bb7d118361
Blake Watters <blakewatters@gmail.com>
2010-09-29 17:57:32 -0400

    Added bundled assets URL cache for replacing web loads with local assets

---
+++ Code/TWTBundledAssetsURLCache.h
@@ -0,0 +1,48 @@
+//
+//  TWTBundledAssetsURLCache.h
+//  TWTCommon
+//
+//  Created by Blake Watters on 9/29/10.
+//  Copyright 2010 Two Toasters. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+/**
+ * Provides a simple substitution cache for returning assets
+ * from the local app bundle in lieu of loading them from a remote
+ * server. Assets that do not have a corresponding local version will
+ * be loaded like normal over the wire.
+ *
+ * Based heavily on code from Matt Gallagher
+ * See http://cocoawithlove.com/2010/09/substituting-local-data-for-remote.html
+ */
+@interface TWTBundledAssetsURLCache : NSURLCache {
+	NSMutableDictionary* _cachedResponses;
+	NSMutableDictionary* _assetMappings;
+	NSMutableDictionary* _MIMETypeMappings;
+}
+
+/**
+ * Returns the asset mappings currently registered with the cache. The
+ * keys are string URL's and the values are file names that exist within the
+ * main bundle
+ */
+@property (nonatomic, readonly) NSMutableDictionary* assetMappings;
+
+/**
+ * Returns the file extension to MIME Type mappings registered with the cache. The
+ * keys are file extensions and the values are the textual MIME Types to return for
+ * files with the given extension loaded from the cache (i.e. 'png' => 'image/png')
+ */
+@property (nonatomic, readonly) NSMutableDictionary* MIMETypeMappings;
+
+/**
+ * Initialize a new URL cache with a dictionary of URL to filename mappings specifying assets in the main bundle to
+ * return instead of actually loading the mapped URL. The MIME type mappings specifies mappings from file extensions
+ * to MIME types (i.e. 'html' => 'text/html', etc).
+ */
+- (id)initWithAssetMappings:(NSDictionary*)URLToFilenameMappings MIMETypeMappings:(NSDictionary*)fileExtensionToMIMETypeMappings;
+
+@end
---
+++ Code/TWTBundledAssetsURLCache.m
@@ -0,0 +1,106 @@
+//
+//  TWTBundledAssetsURLCache.m
+//  TWTCommon
+//
+//  Created by Blake Watters on 9/29/10.
+//  Copyright 2010 Two Toasters. All rights reserved.
+//
+
+#import "TWTBundledAssetsURLCache.h"
+
+@implementation TWTBundledAssetsURLCache
+
+@synthesize assetMappings = _assetMappings;
+@synthesize MIMETypeMappings = _MIMETypeMappings;
+
+- (id)initWithAssetMappings:(NSDictionary*)URLToFilenameMappings MIMETypeMappings:(NSDictionary*)fileExtensionToMIMETypeMappings {
+	if (self = [super init]) {
+		_assetMappings = [[NSMutableDictionary dictionaryWithDictionary:URLToFilenameMappings] retain];
+		_MIMETypeMappings = [[NSMutableDictionary dictionaryWithDictionary:fileExtensionToMIMETypeMappings] retain];
+		_cachedResponses = [[NSMutableDictionary alloc] init];
+	}
+	
+	return self;
+}
+
+- (id)init {
+	if (self = [super init]) {
+		_assetMappings = [[NSMutableDictionary alloc] init];
+		_MIMETypeMappings = [[NSMutableDictionary alloc] init];
+		_cachedResponses = [[NSMutableDictionary alloc] init];
+	}
+	
+	return self;
+}
+
+- (void)dealloc {
+	[_assetMappings release];
+	_assetMappings = nil;
+	[_MIMETypeMappings release];
+	_MIMETypeMappings = nil;
+	[_cachedResponses release];
+	_cachedResponses = nil;
+	
+	[super dealloc];
+}
+
+- (NSString *)mimeTypeForURLString:(NSString *)URLString {
+	NSString* fileExtension = [URLString pathExtension];
+	NSString* MIMEType = [self.MIMETypeMappings objectForKey:fileExtension];
+	if (MIMEType) {
+		return MIMEType;
+	} else {
+		return @"application/octet-stream";
+	}
+}
+
+- (NSCachedURLResponse *)cachedResponseForRequest:(NSURLRequest *)request {
+	// Get the URL for the request
+	NSString* URLString = [[request URL] absoluteString];
+	
+	// See if we have a substitution file for this URL
+	NSString* substitutionFileName = [self.assetMappings objectForKey:URLString];
+	if (NO == substitutionFileName) {
+		return [super cachedResponseForRequest:request];
+	}
+	
+	// If we've already created a cache entry for this path, then return it.
+	NSCachedURLResponse* cachedResponse = [_cachedResponses objectForKey:URLString];
+	if (cachedResponse) {
+		return cachedResponse;
+	}
+	
+	// Get the path to the substitution file
+	NSString* substitutionFilePath = [[NSBundle mainBundle]
+									  pathForResource:[substitutionFileName stringByDeletingPathExtension]
+									  ofType:[substitutionFileName pathExtension]];
+	NSAssert(substitutionFilePath, @"File %@ in substitutionPaths didn't exist", substitutionFileName);
+	
+	// Load the data
+	NSData* data = [NSData dataWithContentsOfFile:substitutionFilePath];
+	
+	// Create the cacheable response
+	// TODO: The extensions...
+	NSURLResponse *response = [[[NSURLResponse alloc] initWithURL:[request URL]
+														 MIMEType:[self mimeTypeForURLString:URLString]
+											expectedContentLength:[data length]
+												 textEncodingName:nil] autorelease];
+	cachedResponse = [[[NSCachedURLResponse alloc] initWithResponse:response data:data] autorelease];
+	
+	// Add it to our cache dictionary
+	[_cachedResponses setObject:cachedResponse forKey:URLString];
+	
+	return cachedResponse;
+}
+
+- (void)removeCachedResponseForRequest:(NSURLRequest *)request {
+	// Get the path for the request
+	NSString* URLString = [[request URL] absoluteString];
+	if ([_cachedResponses objectForKey:URLString]) {
+		[_cachedResponses removeObjectForKey:URLString];
+	} else {
+		[super removeCachedResponseForRequest:request];
+	}
+}
+
+@end
